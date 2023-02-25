d48050e18225f24704f1ea7beea40b49ba7c0f02
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-02-26 00:03:30 +0200

    tes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import vimeoPlayer from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+player.on('pause', function(currentTime) {
+    const videoplayerCurrentTime = currentTime;
+    console.log(videoplayerCurrentTime);
+});
+
+
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
