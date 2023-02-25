09bfd2440245709e5d1a15d343843aa54380ca52
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-02-25 23:07:26 +0200

    first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />\
+        </a>`
+    }).join("");
+}
+
+  const lightbox = new SimpleLightbox(".gallery a", 
+  {
+    captionDelay: 250,
+    captionData: 'alt',
+  });
 
 console.log(galleryItems);
