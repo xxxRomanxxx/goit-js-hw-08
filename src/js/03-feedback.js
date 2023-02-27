913f4727ac02a6394be88581a7e5f245401ff4e9
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-02-27 16:29:16 +0200

    Update 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const STOREGE_KEY = 'feedback-form-state';
+const formData = {};
+const parsedData = JSON.parse(formData);
+console.log(parsedData);
+
+
+const formEl = document.querySelector(".feedback-form");
+const textarea = document.querySelector("textarea");
+const input = document.querySelector("input");
+
+saveInput();
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+    return localStorage.setItem(STOREGE_KEY, JSON.stringify(formData));
+
+
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (input.value === '' || textarea.value === '') 
+  { return alert('Please fill in all the fields!');}
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(STOREGE_KEY);
+}
+
+function saveInput() {
+ const saveData = localStorage.getItem(STOREGE_KEY);
+ console.log(saveData);
+ if (saveData) {
+   console.log(saveData.message);
+ }
+}
